1e28f2b074addaeba88ec4fc77d009e36578538b
Vaishnavi Yeruva <31612562+Yaishnavi@users.noreply.github.com>
2018-01-13 13:01:03 +0530

    Update Hofield train and test.py

--- Hofield train and test.py
+++ Hofield train and test.py
@@ -18,7 +18,7 @@ run: retrieved_output, i = hopfield_retrieval(test_input, weight, n_iter, activa
         weight = trained weight matrix.
         n_iter = choose the maximum number of iterations (very high for continuous case).
         activation = "discrete" or "sigmoid" or "tanh" or "ReLU".
-        The value of input for discrete case is {+1,-1}.
+        The value of input for discrete case should be {+1,-1}. The range of input for sigmoid case should be {0 to 1}
         retrieved_output = The retrieved pattern after n_iter or on reaching stability.
         Asynchronous retrieval. 
         i = total number of iterations.
